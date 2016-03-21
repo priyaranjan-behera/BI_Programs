85ee56c3c21c5aa11ec5b07fa90b78c72f1d33bd
Priyaranjan Behera <pbehera@ncsu.edu>
2016-03-21 17:31:46 -0400

    In between things!

--- 06.Topic-7.Project-6.MarketSegmentation.AttributedGraphCommunityDetection/data/sac1.py
+++ 06.Topic-7.Project-6.MarketSegmentation.AttributedGraphCommunityDetection/data/sac1.py
@@ -10,6 +10,8 @@ import copy
 import scipy
 import numpy as np
 from igraph import *
+import csv
+from collections import defaultdict
 random.seed(0)
 
 
@@ -18,36 +20,45 @@ def main(argv):
 		print("Usage: python sac1.py <alpha>")
 		exit(0)
 
-	alpha = int(argv[0])
+	alpha = float(argv[0])
 	graph = read('fb_caltech_small_edgelist.txt')
-	attributes = np.loadtxt('fb_caltech_small_attrlist.csv', skiprows=1, delimiter=',')
-	#print(attributes)
+	#attributes = np.loadtxt('fb_caltech_small_attrlist.csv', skiprows=1, delimiter=',')
+	attributes = []
+	#print(attributes
+	with open('fb_caltech_small_attrlist.csv') as csvfile:
+		reader = csv.DictReader(csvfile)
+		for row in reader:
+			attributes.append(row)
 
 
 	graph.vs["community"] = range(len(graph.vs))
 	graph.es["weight"] = [1]*len(graph.es)
+
+	for i in range(len(attributes)):
+		for key in attributes[i].keys():
+			graph.vs[i][key] = int(attributes[i][key])
 	#print(graph.get_edgelist())
 	#print(graph.vs["community"])
+	keys = attributes[0].keys()
 
 	for i in range(15):
 		print('Starting Iteration: ', i+1)
 		print "Total vertices", len(graph.vs)
 
-
+		print graph.vs
 		editsince = 0;
 		while(1):
 			vertex = random.randint(0, len(graph.vs)-1)
-			print('Processing Random Vertex: i+1', vertex)
+			print('Processing Random Vertex:', i+1, vertex)
 			cm_gain = 0;
 			edit = 0;
-			editsince = 0;
 			#if(vertex%20 == 0):
 			#	print 'Vertex:',vertex
 
 			new_community = graph.vs[vertex]['community']
 			old_community = graph.vs[vertex]['community']
 			for community in np.unique(graph.vs["community"]):
-				temp_cm_gain = getCompositeModularityGain(graph, community, vertex, attributes, alpha)
+				temp_cm_gain = getCompositeModularityGain(graph, community, vertex, keys, alpha)
 				if(temp_cm_gain > cm_gain):
 					new_community = community
 					cm_gain = temp_cm_gain
@@ -57,9 +68,12 @@ def main(argv):
 				editsince = 0;
 			else:
 				editsince += 1
-				if(editsince > 3):
+				if(editsince > 5):
 					break;
 
+			print('editsince = ', editsince)
+
+			print 'Moving from: ', graph.vs[vertex]['community'], 'to: ', new_community
 			graph.vs[vertex]['community'] = new_community
 
 		print("Total number of communities is: ", len(np.unique(graph.vs["community"])))
@@ -67,10 +81,20 @@ def main(argv):
 
 
 		#Phase2 Begins
-		graph2 = graph.contract_vertices(graph.vs["community"], combine_attrs=dict(weight="sum", community="first"))
+		print "Start of Phase2"
+		communityList = graph.vs["community"]
+		graph.contract_vertices(communityList, combine_attrs=mean)
+		graph.simplify(combine_edges=sum)
+		#graph.vs["community"] = communityList
 
-		graph = graph2
 
+
+		print "After Phase 2"
+
+		print('The Edgelist is: ',graph.get_edgelist())
+		print('Communities are: ', graph.vs["community"])
+
+		
 
 
 		for community in np.unique(graph.vs["community"]):
@@ -92,15 +116,22 @@ def main(argv):
 	#plot(graph, 'communities.pdf', layout=layout)
 
 
-def getCompositeModularityGain(graph, community, newPoint, attributes, alpha):
+def getCompositeModularityGain(graph, community, newPoint, keys, alpha):
 	communityList = graph.vs["community"]
 	structural_modularity_old = graph.modularity(communityList)
 
 	comm_size = 0;
 	sim_sum = 0;
 	for index in range(len(graph.vs)):
-		if(graph.vs[index] == community):
-			similarity = scipy.spatial.distance.cosine(attributes[index], attributes[newPoint])
+		if(graph.vs[index]['community'] == community):
+			list1 = []
+			list2 = []
+
+			for key in keys:
+				list1.append(graph.vs[index][key])
+				list2.append(graph.vs[newPoint][key])
+			similarity = 1-scipy.spatial.distance.cosine(list1, list2)
+			#print 'similarity =', similarity 
 			sim_sum += similarity
 			comm_size += 1
 
@@ -114,6 +145,11 @@ def getCompositeModularityGain(graph, community, newPoint, attributes, alpha):
 	DeltaQNewmann = structural_modularity_new - structural_modularity_old
 
 	DeltaQ = (alpha * DeltaQNewmann) + ((1-alpha) * DeltaQAttr)
+
+	#print 'For community = ', community
+	#print 'DeltaQNewmann =',DeltaQNewmann
+
+	#print 'DeltaQAttr =',DeltaQAttr 
 
 	return DeltaQ
 
